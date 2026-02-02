62e6c39260bf7379028aa77f76037d4750592488
Sergio Suraci <sergio.suraci90@gmail.com>
2026-02-02 17:20:13 +0100

    api call

---
+++ app/api/games/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const apiKey = process.env.API_KEY;
+  if (!apiKey) {
+    return NextResponse.json({ error: "API_KEY mancante" }, { status: 500 });
+  }
+
+  const searchParams = new URLSearchParams();
+  searchParams.set("key", apiKey);
+  searchParams.set("page", '1');
+
+  
+  // chiamata all'API esterna con la key
+  const upstream = await fetch(`https://api.rawg.io/api/games?${searchParams.toString()}`, {
+    cache: "no-store",
+  });
+
+  if (!upstream.ok) {
+    return NextResponse.json({ error: `Upstream ${upstream.status}` }, { status: 502 });
+  }
+
+  const data = await upstream.json();
+  return NextResponse.json(data);
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,22 @@
-import { Carousel } from "../components/Carousel/Carousel";
+"use client";
+import { useEffect } from "react";  
 
 export default function Dashboard(){
+
+ 
+  const loadGames = async () => {
+    const res = await fetch(`/api/games`);
+    const data = await res.json();
+    return data.results;
+  }
+
+  useEffect(() => {
+    loadGames().then(games => {
+      console.log(games);
+    });
+  }, []);
+
   return(
+    <div></div>
   )
 }
